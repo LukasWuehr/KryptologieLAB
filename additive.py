423ee69cc676e1f04648c22fa97cabf49d99e9cc
LukasWuehr <lukas.wuehrggmx.de>
2020-06-25 18:58:20 +0200

    formatting, nicer names

--- additive.py
+++ additive.py
@@ -1,52 +1,54 @@
-import sys, getopt
+import getopt
+import sys
 
-alphabet='ABCDEFGHIJKLMNOPQRSTUVWXYZ' #used alphabet
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'  # used alphabet
+
 
 def main(argv):
-   try:
-      opts, args = getopt.getopt(argv,"hedb:",["encrypt=","decrypt=","bruteforce="]) #input arguments
-   except getopt.GetoptError: #error
-      print( 'additive.py -[e,d] <inputfile>  <outputfile> <key>')
-      sys.exit(2)
+    try:
+        opts, args = getopt.getopt(argv, "hedb:", ["encrypt=", "decrypt=", "bruteforce="])  # input arguments
+    except getopt.GetoptError:  # error
+        print('additive.py -[e,d] <inputfile>  <outputfile> <key>')
+        sys.exit(2)
 
-   inputFile = open(argv[1], 'r')
-   inputText = inputFile.read()
-   inputFile.close()
-   outputText=''
+    input_file = open(argv[1], 'r')
+    input_text = input_file.read()
+    input_file.close()
+    output_text = ''
 
-   for opt, arg in opts:   #choose the option
-      if opt == '-h':      #help
-         print('additive.py -[e,d] <inputfile>  <outputfile> <key>')
-         sys.exit()
-      elif opt in ("-e", "--encrypt"): #encrypt
-         outputText = encrypt(argv[3],inputText)
-      elif opt in ("-d", "--decrypt"): #decrypt
-         outputText = decrypt(argv[3],inputText)
-      elif opt in ("-b", "--bruteforce"): #all possible keys
-         for key in range(1,len(alphabet)):
-            outputText += str(key)+": "+decrypt(key, inputText)+"\n"
+    for opt, arg in opts:  # choose the option
+        if opt == '-h':  # help
+            print('additive.py -[e,d] <inputfile>  <outputfile> <key>')
+            sys.exit()
+        elif opt in ("-e", "--encrypt"):  # encrypt
+            output_text = encrypt(argv[3], input_text)
+        elif opt in ("-d", "--decrypt"):  # decrypt
+            output_text = decrypt(argv[3], input_text)
+        elif opt in ("-b", "--bruteforce"):  # all possible keys
+            for key in range(1, len(alphabet)):
+                output_text += str(key) + ": " + decrypt(key, input_text) + "\n"
 
-      print(outputText) #writing
-      outputFile = open(argv[2], 'w')
-      outputFile.write(outputText)
-      outputFile.close()
+        print(output_text)  # writing
+        outputFile = open(argv[2], 'w')
+        outputFile.write(output_text)
+        outputFile.close()
 
 
-def encrypt(key,text): #encryption of text
-   rotateAlphabet=''
-   length = len(alphabet)
-   for numb in range (0,length): #create shifted alphabet
-      rotateAlphabet+=alphabet[(numb+int(key))%length]
-   return text.translate(str.maketrans(alphabet, rotateAlphabet)) #translation with table
+def encrypt(key, text):  # encryption of text
+    rotate_alphabet = ''
+    length = len(alphabet)
+    for numb in range(0, length):  # create shifted alphabet
+        rotate_alphabet += alphabet[(numb + int(key)) % length]
+    return text.translate(str.maketrans(alphabet, rotate_alphabet))  # translation with table
 
-def decrypt(key,text): #decryption of text
-   rotateAlphabet=''
-   length = len(alphabet)
-   for numb in range (0,length): #create shifted alphabet
-      rotateAlphabet+=alphabet[(numb-int(key))%length]
-   return text.translate(str.maketrans(alphabet, rotateAlphabet)) #translation with table
+
+def decrypt(key, text):  # decryption of text
+    rotate_alphabet = ''
+    length = len(alphabet)
+    for numb in range(0, length):  # create shifted alphabet
+        rotate_alphabet += alphabet[(numb - int(key)) % length]
+    return text.translate(str.maketrans(alphabet, rotate_alphabet))  # translation with table
+
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
-email = "lukas.wuehr@uni-jena.de"
-print(email[7]+email[8])
+    main(sys.argv[1:])
