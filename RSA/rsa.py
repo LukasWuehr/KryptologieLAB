06df876ffe7a805ce19f7f8f2c074a8b2c7a5283
LukasWuehr <lukas.wuehrggmx.de>
2020-08-30 18:06:53 +0200

    different de-&encryption

--- RSA/rsa.py
+++ RSA/rsa.py
@@ -9,24 +9,32 @@ def key(file):
     return n, k
 
 
-def text(file):
+def text(file, chunk):
     with open(file, 'rb')as text_file:
-        m = text_file.read(8)
-        while len(m) == 8:
+        m = text_file.read(chunk)
+        while len(m) == chunk:
             yield int.from_bytes(m, 'big')
-            m = text_file.read(8)
+            m = text_file.read(chunk)
         if len(m) > 0:
-            yield int.from_bytes(m, 'big') << ((8 - len(m)) * 8)
+            yield int.from_bytes(m + b' ' * (chunk - len(m)), 'big')
 
 
-def cypher(file, dest_file, n, k):
-    text_gen = text(file)
+def encrypt(file, dest_file, n, k):
+    text_gen = text(file, 4)
     if os.path.exists(dest_file):  # begin with empty file
         os.remove(dest_file)
     with open(dest_file, 'ab') as dest:
         for m in text_gen:
-            print(m.to_bytes(8, 'big'))
-            dest.write(exp2(m,k,n).to_bytes(8,'big'))
+            dest.write(pow(m, k, n).to_bytes(8, 'big'))
+
+
+def decrypt(file, dest_file, n, k):
+    text_gen = text(file, 8)
+    if os.path.exists(dest_file):  # begin with empty file
+        os.remove(dest_file)
+    with open(dest_file, 'ab') as dest:
+        for m in text_gen:
+            dest.write(pow(m, k, n).to_bytes(4, 'big'))
 
 
 def exp(m, k, n):
@@ -37,13 +45,14 @@ def exp(m, k, n):
         e += 1
     return c
 
+
 def exp2(m, k, n):
     x = 1
     while k != 0:
         if k & 1 == 1:
-            x = (x*m) % n
+            x = (x * m) % n
         k >>= 1
-        m = m**2 % n
+        m = m ** 2 % n
     return x
 
 
@@ -51,6 +60,6 @@ if __name__ == '__main__':
     print(key('prvate.txt'))
     print(key('public.txt'))
     n, k = key('public.txt')
-    cypher('../DES/text.txt','crypto.txt', n,k)
+    encrypt('../DES/text.txt', 'crypto.txt', n, k)
     n, d = key('prvate.txt')
-    cypher('crypto.txt', 'decrypto.txt', n, d)
+    decrypt('crypto.txt', 'decrypto.txt', n, d)
